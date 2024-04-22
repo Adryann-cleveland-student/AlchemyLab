e6d5c7906e274d877d2c6406d0b45b5990a9b99f
Adryann/Ace <adcld66@student.oaisd.org>
2024-04-22 10:43:33 -0400

    push

--- Assets/Scripts/PickUP.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PickUP : MonoBehaviour
-{
-    public GameObject player;
-    public Transform holdPos;
-}
